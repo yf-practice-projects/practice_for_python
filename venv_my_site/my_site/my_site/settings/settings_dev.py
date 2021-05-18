812254a5f935ae5ef9fd2358c13a817483eb65ee
yf-portfolio <yoshiki_fujishima@rosso-tokyo.co.jp>
2021-05-18 16:10:14 +0900

    [create]settings file
    開発用と本番用

--- venv_my_site/my_site/my_site/settings/settings_dev.py
+++ venv_my_site/my_site/my_site/settings/settings_dev.py
@@ -4,3 +4,37 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+LOGGING = {
+	'version':1,
+	'disable_existing_loggers':False,
+	
+	'loggers':{
+		'django':{
+			'handlers':['console'],
+			'level':'INFO',
+		},
+
+		'which_one':{
+			'handlers':['console'],
+			'level':'DEBUG',
+		}
+	},
+	'handlers':{
+		'console':{
+			'level':'DEBUG',
+			'class':'logging.StreamHandler',
+			'formatter':'dev'
+		}		
+	},
+	'formatters':{
+		'dev':{
+			'format':'\t'.join([
+				'%(asctime)s',
+				'[%(levelname)s]',
+				'%(pathname)s(Line:%(lineno)d)',
+				'%(message)s'
+			])
+		}
+	}
+}
--- venv_my_site/my_site/my_site/settings/settings_pro.py
+++ venv_my_site/my_site/my_site/settings/settings_pro.py
@@ -3,4 +3,44 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = []
+
+LOGGING = {
+	'version':1,
+	'disable_existing_loggers':False,
+	
+	'loggers':{
+		'django':{
+			'handlers':['file'],
+			'level':'INFO',
+		},
+
+		'which_one':{
+			'handlers':['file'],
+			'level':'INFO',
+		}
+	},
+
+	'handlers':{
+		'console':{
+			'level':'INFO',
+			'class':'logging.handlers.TimedRotatingFileHandler',
+			'filename':os.path.join(BASE_DIR,'logs/django.log'),
+			'formatter':'pro',
+			'when':'D',
+			'interval':1,
+			'backupcount':7,
+		}
+	},
+
+	'formatters':{
+		'pro':{
+			'format':'\t'.join([
+				'%(asctime)s',
+				'[%(levelname)s]',
+				'%(pathname)s(Line:%(lineno)d)',
+				'%(message)s'
+			])
+		}
+	}
+}
