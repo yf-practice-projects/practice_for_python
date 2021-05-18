57f578983fbda4d33c644548dba7129a5e824e97
yf-portfolio <yoshiki_fujishima@rosso-tokyo.co.jp>
2021-05-18 16:19:26 +0900

    [update]settings_pro
    ALLOWED_HOSTS追記

--- venv_my_site/my_site/my_site/settings/settings_pro.py
+++ venv_my_site/my_site/my_site/settings/settings_pro.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',os.environ.get('ALLOWED_HOSTS')]
 
 LOGGING = {
 	'version':1,
